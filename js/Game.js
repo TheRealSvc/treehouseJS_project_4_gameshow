3c4ffa460285f744d163162fab29e94fe4fb3858
SVC <svcoelln@yahoo.de>
2021-03-05 23:05:07 +0100

    sjg

--- js/Game.js
+++ js/Game.js
@@ -8,20 +8,18 @@ class Game {
         this.phrases = phrases ;
         this.missed = 0 ;
         this.activePhrase = new Phrase(this.getRandomPhrase()) ;
-        console.log(`active phrase is: ${this.activePhrase.phrase}`)
+        this.keyupCount = 0 ;
     }
 
     startGame(phrases=this.phrases) {
         const gameSection = document.querySelector('#overlay') ;
         gameSection.style.display = 'none' ;
         this.missed = 0 ;    
-        this.handleInteraction() ;
     } ;
     
     getRandomPhrase() {
        return this.phrases[Math.min(Math.floor(Math.random()*this.phrases.length),this.phrases.length)] ;
     }
-
 
     handleInteraction() {
         var qwertyEl = document.querySelector('#qwerty') ;
@@ -29,28 +27,36 @@ class Game {
             if (e.target.className === "key") {  // this is order not to count "keyrow clicks" 
             e.target.disabled = true ; // directly disable the display-keyboard button
             console.log(`click callback called ${e.target}`)
-            this.checkLetter(e.target.textContent, e.target) ; // here if wrong the keyboard letter will be marked as wrong
+            this.activePhrase.checkLetter(e.target.textContent, e.target) ; // here if wrong the keyboard letter will be marked as wrong
             } else { } ; // reminder to clean up 
-            }) ;
+            this.checkForWin() ;   
+        }) ;
         
-            document.addEventListener('keyup', (ee) => {
-                console.log(`keyup callback called ${ee.target}`)
-            if (/[a-z]/.test(ee.key)) { // react only when a letter (lowercase) is pressed 
-                for(var i=0; i<qwertyEl.children.length; i++) {
-                    var subEl = qwertyEl.children[i] ; 
-                    for (var j=0 ; j < subEl.children.length ; j++ ) {
-                        if (subEl.children[j].textContent === ee.key) {
-                            subEl.children[j].disabled = true ; // this disables the display-keyboard button after keyborad input  
-                            this.checkLetter(ee.key,subEl.children[j]) ;  // here if wrong the keyboard letter will be marked as wrong
-                            break ;
-                        }
-                    }
-                } 
+        document.addEventListener('keyup', (ee) => {
+            console.log(`keyup callback called ${ee.target}`) ;
+            if(this.keyupCount == 0) { // couldnt stop keyup event from firing twice. This is some workaround.  
+               this.keyupCount = 1 ;
+                if (/[a-z]/.test(ee.key)) { // react only when a letter (lowercase) is pressed 
+                    for(var i=0; i<qwertyEl.children.length; i++) {
+                        var subEl = qwertyEl.children[i] ; 
+                        for (var j=0 ; j < subEl.children.length ; j++ ) {
+                         if (subEl.children[j].textContent === ee.key) {
+                          subEl.children[j].disabled = true ; // this disables the display-keyboard button after keyborad input  
+                          flag = this.activePhrase.checkLetter(ee.key,subEl.children[j]) ;  // here if wrong the keyboard letter will be marked as wrong
+                          break ;
+                         }
+                     }                      
+                 } 
+                }
+            } else { 
+                this.keyupCount = 0 ; 
             } 
-        },false ) ; 
+            this.checkForWin() ;
+            console.log(this.missed) ;
+        }) ; 
     }  
 
-    removeLife() {
+    static removeLife() {
         var heartsOl = document.querySelector("#scoreboard").children[0] ; 
         for (var i=heartsOl.children.length-1; i >= 0; i-=1) { // replace right heart first 
             var heartsImg = heartsOl.children[i].children ;
@@ -58,40 +64,28 @@ class Game {
                 heartsImg[0].outerHTML = '<img src="images/lostHeart.png" alt="Heart Icon" height="35" width="30">' ;
                 break ;
             }
+        }
+        //this.missed += 1 ; 
+        //console.log(`missed is ${this.missed}`);
+    } 
+
+    checkForWin() {
+        if (this.missed>=5 ) { 
+            this.gameOver() ;
         } 
-    } 
-
-    showMatchedLetter(letter,i) {
-        var phraseUlAll = document.querySelector("#phrase").children[0].children ; 
-        phraseUlAll[i].classList.add('chosen') ;           
-    } 
-
-    checkForWin(letter) {
     }; 
 
-    checkLetter(letter, keyEl) {
-        console.log(keyEl)
-        var phraseUlAll = document.querySelector("#phrase").children[0].children ; 
-        var flag = false ;
-        if (letter.length == 1) { 
-         for (var i=0; i<phraseUlAll.length; i++) {
-           if(phraseUlAll[i].innerText.toLowerCase() == letter) {   
-            this.showMatchedLetter(letter,i) ; // maybe i change this back and remove the showMathedLetter  
-            flag = true ;
-           } 
-         }
-         if (!flag) {
-             this.missed += 1 ;
-             console.log(this.missed) ;
-             keyEl.classList.add('wrong') ;
-             this.removeLife() ;
-           }
-        } 
-    } 
-
+ /*
+ Includes gameOver() method that displays a final "win" or "loss" message by showing the original start screen
+overlay styled with either the win or lose CSS class
+ */
     gameOver() {
         const gameSection = document.querySelector('#overlay') ;
         gameSection.style.display = 'block' ;
+        if(this.missed >=5) {
+            console.log("you lost !!!!!!! ") ;
+            location.reload() ;
+        } 
     } ;
 
 }
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,6 +1,8 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+//
+
 class Phrase {
     constructor(phrase) {
         this.phrase=phrase ;
@@ -21,13 +23,32 @@ class Phrase {
     } 
    } ;
 
-   checkLetter() {
 
-   };
-   showMatchedLetters()  {
+checkLetter(letter, keyEl) {
+    console.log(keyEl)
+    var phraseUlAll = document.querySelector("#phrase").children[0].children ; 
+    var flag = false ;
+    if (letter.length == 1) { 
+        for (var i=0; i<phraseUlAll.length; i++) {
+           if(phraseUlAll[i].innerText.toLowerCase() == letter) {   
+            this.showMatchedLetter(letter,i) ; // maybe I change this back and remove the showMathedLetter  
+            flag = true ;
+           } 
+         }
+         if (!flag) {
+             //this.missed += 1 ;
+             //console.log(this.missed) ;
+             keyEl.classList.add('wrong') ;
+             Game.removeLife() ; // iam calling a static method of Game
+           }
+        }
+    return flag ;
+} ;
 
-   } ;
 
+showMatchedLetter(letter,i) {
+    var phraseUlAll = document.querySelector("#phrase").children[0].children ; 
+    phraseUlAll[i].classList.add('chosen') ;           
 }
 
-// <li class="hide letter h">h</li>
+} // end of class 
